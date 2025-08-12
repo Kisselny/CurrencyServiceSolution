d1fbcab41fcce9ac2875a1c5218fd67c5c822d74
Dmitry Trukhanov <eareell@gmail.com>
2025-08-12 21:43:10 +0300

    пошла жара. делаем чистую архитектуру

---
+++ CurrencyUpdaterService.Infrastructure/External/CurrencyDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyUpdaterService.Infrastructure.External;
+
+public class CurrencyDto
+{
+    public string Id { get; set; } = null!;
+    public string CharCode { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public int Nominal { get; set; }
+    public decimal Value { get; set; }
+
+}
---
+++ CurrencyUpdaterService.Infrastructure/External/ICurrencyApiClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace CurrencyUpdaterService.Infrastructure.External;
+
+public interface ICurrencyApiClient
+{
+    Task<IEnumerable<CurrencyDto>> FetchCurrenciesAsync(CancellationToken ct);
+}
+
+public class CurrencyApiClient : ICurrencyApiClient
+{
+    private readonly HttpClient _http;
+
+    public CurrencyApiClient(HttpClient http) => _http = http;
+    
+    public async Task<IEnumerable<CurrencyDto>> FetchCurrenciesAsync(CancellationToken ct)
+    {
+        var url = "http://www.cbr.ru/scripts/XML_daily.asp";
+        var xml = await _http.GetStringAsync(url, ct);
+        var doc = XDocument.Parse(xml);
+
+        return new List<CurrencyDto>();
+
+    }
+}
