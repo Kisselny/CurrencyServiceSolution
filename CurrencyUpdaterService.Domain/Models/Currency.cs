49b505bd20b5dc3ddf6d057ff5cf12f25f77742e
Dmitry Trukhanov <eareell@gmail.com>
2025-08-13 16:57:57 +0300

    Добавлен домен и модель валюты

---
+++ CurrencyUpdaterService.Domain/Models/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace CurrencyUpdaterService.Domain.Models;
+
+public class Currency
+{
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal Rate { get; set; }
+}
