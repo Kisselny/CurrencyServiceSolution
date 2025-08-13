4c5e6181481c117906ca796f802c1be3a61ee200
Dmitry Trukhanov <eareell@gmail.com>
2025-08-13 23:16:39 +0300

    Добавлен ДБ-контекст

--- CurrencyUpdaterService.Worker/Program.cs
+++ CurrencyUpdaterService.Worker/Program.cs
@@ -11,6 +11,9 @@ namespace CurrencyUpdaterService.Worker
             builder.Services.AddHostedService<Worker>();
             builder.Services.AddSingleton<ICurrencyApiClient, CurrencyApiClient>();
             builder.Services.AddHttpClient();
+            builder.Services.AddDbContext<CurrencyDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             var host = builder.Build();
             host.Run();
         }
