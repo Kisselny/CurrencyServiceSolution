a0295c11a9282cb191adf7453b251b1e721df2a4
Dmitry Trukhanov <eareell@gmail.com>
2025-08-16 15:20:04 +0300

    Начат микросервис курсов валют
    (cherry picked from commit 51adb97f039dfa18b0131f0ad8e827a115dd6608)

---
+++ CurrencyService.Domain/Models/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace CurrencyService.Domain;
+
+public class Currency
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Rate { get; set; }
+}
